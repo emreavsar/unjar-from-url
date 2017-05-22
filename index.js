29ad01643752c2d79a306a33c542bd7b5bd064e7
Emre Avsar <emre@kuhodhcp-030.k.arpage.ch>
2017-05-22 09:06:24 +0200

    implemented cli argument support

--- index.js
+++ index.js
@@ -3,17 +3,127 @@
 /**
  * Downloads a list of jar files, and unzippes them inside the node_modules directory.
  * @return {boolean} result when download was ok
+ * @author Emre Avsar <emre.avsar92@gmail.com>
  */
- module.exports = function() {
+module.exports = function () {
+	var unjarConfigFromPackageJsonFile = getUnjarConfigurationFromPackageJson(module);
+	// var unjarConfigFromArguments = getUnjarConfigurationFromArguments(args);
 
- 	var highestParent = module.parent;
- 	var parentFound = false;
+	var unjarConfigFromArguments = getUnjarConfigurationFromArgs();
+	// put both configurations together:
+	var unjarConfig = unjarConfigFromArguments.concat(unjarConfigFromPackageJsonFile);
+
+	for (var i = 0; i < unjarConfig.length; i++) {
+		downloadJar(unjarConfig[i]['nodeModulesDir'], unjarConfig[i]['directory'], unjarConfig[i]['url']);
+	}
+
+	return
+};
+
+/**
+ * Finds the configuration passed by command line arguments.
+ * 
+ * CLI args are limited, thus one have to pass the following CLI arguments:
+ * 	--urls = list of urls to download the jars from
+ *  --download-dirs = names of the directories to put the jars into (uncompress jar into this directory)
+ *  --destination-dirs = names of the directories where the directory (download-dirs) will be
+ * 
+ * Example for usage with command line args:
+ * <pre><code>
+ * --urls http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar 
+ * --download-dirs selenium-server-standalone 
+ * --destination-dirs=/data
+ * --urls http://otherurl.com/file.jar 
+ * --download-dirs otherurl-directory 
+ * --destination-dirs=/tmp 
+ * </pre></code>
+ * 
+ * This would download the selenium server standalone 2.4.32.1 jar into /data/selenium-server-standalone/  and uncompress the jar there 
+ * and file.jar downloaded from http://otherurl.com/file.jar into /tmp/otherurl-directory/  and uncompress the jar there
+ * 
+ * Important: the order does matter (first urls argument matches to first download-dirs and destination-dirs etc.)
+ */
+var getUnjarConfigurationFromArgs = function () {
+	// looking for keys passed as cli arguments with urls and download-dirs
+	const urlsKey = "urls";
+	const downloadDirsKey = "download-dirs";
+	const destinationDir = "destination-dirs";
+
+	// keys used in the array (same as in configuration via package.json) for each item (so urls => url)
+	const confUrlsKey = "url";
+	const confDownloadDirsKey = "directory";
+	const confDestinationDir = "nodeModulesDir";
+
+	// getting cli arguments
+	var argv = require('minimist')(process.argv.slice(2));
+
+	var unjarConfigFromArguments = [];
+
+	if (argv[urlsKey] != null) {
+		// only 1 object passed
+		if (typeof argv[urlsKey] === 'string') {
+			// put into an array, so as it can be used the same way as multiple args
+			argv[urlsKey] = [argv[urlsKey]];
+			argv[downloadDirsKey] = [argv[downloadDirsKey]];
+			argv[destinationDir] = [argv[destinationDir]];
+		}
+
+		// put them together so that the array has a object with the attributes: nodeModulesDir, directory, url
+		for (var i = 0; i < argv[urlsKey].length; i++) {
+			var configItem = {};
+			configItem[confUrlsKey] = argv[urlsKey][i];
+			configItem[confDownloadDirsKey] = argv[downloadDirsKey][i];
+			configItem[confDestinationDir] = argv[destinationDir][i];
+			unjarConfigFromArguments.push(configItem);
+		}
+	}
+
+	return unjarConfigFromArguments;
+}
+
+/**
+ * Searches the unjar configuration inside the highest parent's package.json. This is typically used when this module is used inside another project
+ * which has the configuration inside the package.json of the project itself.
+ * 
+ * Returns unjar configuration as a js object.
+ * 
+ * Example for configuration:
+ * <pre><code>
+ * // package.json of your project
+ * ...
+ * unjarConfig: [
+ * 		{
+ * 			"directory": "selenium-server-standalone",
+ * 			"url": "http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar"
+ * 		},
+ * 		{
+ * 			// other jars
+ * 		}
+ * ]
+ * </pre></code>
+ * 
+ * @param {module} module to start searching the configuration file 
+ */
+var getUnjarConfigurationFromPackageJson = function (module) {
+	if (module == null) {
+		return [];
+	}
+
+	var highestParent;
+	if (module.parent != null) {
+		highestParent = module.parent;
+	} else {
+		highestParent = module;
+	}
+
+
+	var parentFound = false;
 
 	// iterate up to all parents, until parent is undefined => root of all
-	while(!parentFound) {
-		parentFound = highestParent.parent == undefined;
+	while (!parentFound) {
+		parentFound = highestParent.parent == undefined || highestParent.parent == null;
 		// go to upper parent
-		if(!parentFound) {
+		if (!parentFound) {
 			highestParent = highestParent.parent;
 		}
 	}
@@ -24,15 +134,21 @@
 	// read the package json
 	var packageJson = require(pathToNodeModules + "package.json");
 
-
 	var unjarFromUrlConfig = packageJson['config'];
 
-	for(var i = 0; i<unjarFromUrlConfig.length; i++) {
-		downloadJar(highestParent.paths[0], unjarFromUrlConfig[i]['directory'], unjarFromUrlConfig[i]['url']);
+	if (unjarFromUrlConfig != null) {
+		// add the highest parent path (used as nodeModulesDir later) to each item
+		var nodeModulesDir = highestParent.paths != null ? highestParent.paths[0] : '';
+		unjarFromUrlConfig.forEach(function (item) {
+			item.nodeModulesDir = nodeModulesDir
+		});
+	} else {
+		console.warn("No config found in package.json: ", pathToNodeModules + "package.json");
+		unjarFromUrlConfig = [];
 	}
 
-	return
-};
+	return unjarFromUrlConfig;
+}
 
 /**
  * Downloads and uncompresses a jar from given url into node_modules directory.
@@ -40,11 +156,12 @@
  * @param {string} unzipDirectory name of the directory where the jar file gets uncompressed in
  * @param {string} url to jar to download
  */
-var downloadJar = function(nodeModulesDir, unzipDirectory, url) {
+var downloadJar = function (nodeModulesDir, unzipDirectory, url) {
 	const download = require('download');
 	const fs = require('fs');
 	const zlib = require('zlib');
-	var exec = require('child_process').exec,child;
+	var exec = require('child_process').exec,
+		child;
 	var path = require("path");
 
 	// extracting filename from url
@@ -54,10 +171,11 @@ var downloadJar = function(nodeModulesDir, unzipDirectory, url) {
 	console.log("nodeModules directory = ", nodeModulesDir);
 	console.log("downloading file from url=", url);
 	console.log("unzipping to directory=", unzipDirectory);
+	console.log("fileDriectory=", fileDirectory);
 	console.log("filename = ", filename);
 
 	// makes programming easier (create the directory anyway)
-	if(!fs.existsSync(fileDirectory)) {
+	if (!fs.existsSync(fileDirectory)) {
 		fs.mkdirSync(fileDirectory);
 	}
 
@@ -95,3 +213,5 @@ var downloadJar = function(nodeModulesDir, unzipDirectory, url) {
 		});
 	});
 };
+
+module.exports()
