985e084a8bf329bc647fb80b720f01a5f656f673
Emre Avsar <emre@kuhodhcp-030.k.arpage.ch>
2017-05-22 09:20:06 +0200

    removed initial tests, will commit soon (haha)

--- test/test.js
+++ test/test.js
@@ -4,10 +4,5 @@ var expect = require('chai').expect;
 var unjar = require('../index');
 
 describe('#unjar', function() {
-    it('should download the file', function() {
-        var result = unjar([{
-            "dirToPut": "myurl.jar"
-        }]);
-        expect(result).to.equal(1);
-    });
+    // todo
 });
